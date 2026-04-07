30ddc45cf192121beca3e5b411791d3bdba41a8f
Ilya <illya.paralynov@gmail.com>
2026-04-07 16:20:36 +0000

    lab07 finished

---
+++ lab07/private/CityObject.cpp
@@ -0,0 +1,21 @@
+#include "CityObject.hpp"
+#include <iostream>
+
+CityObject::CityObject(int id, const std::string &name, const std::string &location)
+    : id_(id), name_(name), location_(location) {}
+
+int CityObject::getId() const {
+    return id_;
+}
+
+std::string CityObject::getName() const {
+    return name_;
+}
+
+std::string CityObject::getLocation() const {
+    return location_;
+}
+
+void CityObject::describe() const {
+    std::cout << "CityObject: " << name_ << " located at " << location_ << std::endl;
+}
