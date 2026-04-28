735f0947341a828267595284668bd90a88017cde
Ilya <illya.paralynov@gmail.com>
2026-04-28 15:53:21 +0000

    lab10 initial

---
+++ lab10/tests/tests_variant_utils.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+#include "variant_utils.hpp"
+
+using namespace GeometryUtils;
+
+// -------------------------
+// square()
+// -------------------------
+TEST(GeometryUtilsTest, SquareInt) {
+    EXPECT_EQ(square(5), 25);
+    EXPECT_EQ(square(-4), 16);
+}
+
+TEST(GeometryUtilsTest, SquareDouble) {
+    EXPECT_DOUBLE_EQ(square(2.5), 6.25);
+}
+
+// -------------------------
+// midpoint()
+// -------------------------
+TEST(GeometryUtilsTest, MidpointInt) {
+    EXPECT_EQ(midpoint(2, 6), 4);
+}
+
+TEST(GeometryUtilsTest, MidpointMixed) {
+    EXPECT_DOUBLE_EQ(midpoint(2, 5.0), 3.5);
+}
+
+TEST(GeometryUtilsTest, MidpointNegative) {
+    EXPECT_DOUBLE_EQ(midpoint(-4, 4), 0.0);
+}
+
+// -------------------------
+// distance1D()
+// -------------------------
+TEST(GeometryUtilsTest, DistanceInt) {
+    EXPECT_EQ(distance1D(3, 7), 4);
+}
+
+TEST(GeometryUtilsTest, DistanceNegative) {
+    EXPECT_EQ(distance1D(-5, 3), 8);
+}
+
+TEST(GeometryUtilsTest, DistanceCoincident) {
+    EXPECT_EQ(distance1D(4, 4), 0);
+}
+
+TEST(GeometryUtilsTest, DistanceMixed) {
+    EXPECT_DOUBLE_EQ(distance1D(2, 5.5), 3.5);
+}
